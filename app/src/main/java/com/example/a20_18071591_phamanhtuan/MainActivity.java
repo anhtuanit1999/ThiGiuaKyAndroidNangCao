da6aadd632cb534b1b85de6c7d00179fcdfc1261
AnhTuanIT <ngocdungbb1@gmail.com>
2021-10-15 16:00:47 +0700

    cau 3

--- app/src/main/java/com/example/a20_18071591_phamanhtuan/MainActivity.java
+++ app/src/main/java/com/example/a20_18071591_phamanhtuan/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a20_18071591_phamanhtuan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imgLoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgLoa = findViewById(R.id.imgLoa);
+        Animation animation =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.aniblink);
+
+        imgLoa.startAnimation(animation);
     }
 }
